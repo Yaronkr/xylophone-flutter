c7528bf95f1e068a307b103252558605dbdb88d9
Yaronkr <yaron.kr@outlook.com>
2022-07-05 14:16:19 +1000

    sound functions

--- lib/main.dart
+++ lib/main.dart
@@ -10,85 +10,36 @@ class XylophoneApp extends StatelessWidget {
     AssetsAudioPlayer.newPlayer().open(Audio("assets/note$soundNumber.wav"));
   }
 
+  Expanded buildKey({color, soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: Container(
+          padding: EdgeInsets.all(0.0),
+          color: color,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
         body: SafeArea(
           child: Center(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                TextButton(
-                  onPressed: () {
-                    playSound(1);
-                  },
-                  child: Container(
-                    height: 50,
-                    width: 200,
-                    color: Color.fromARGB(255, 245, 75, 75),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    playSound(2);
-                  },
-                  child: Container(
-                    height: 50,
-                    width: 200,
-                    color: Color.fromARGB(255, 235, 243, 8),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    playSound(3);
-                  },
-                  child: Container(
-                    height: 50,
-                    width: 200,
-                    color: Color.fromARGB(255, 3, 235, 100),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    playSound(4);
-                  },
-                  child: Container(
-                    height: 50,
-                    width: 200,
-                    color: Color.fromARGB(255, 4, 84, 243),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    playSound(5);
-                  },
-                  child: Container(
-                    height: 50,
-                    width: 200,
-                    color: Color.fromARGB(255, 255, 1, 242),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    playSound(6);
-                  },
-                  child: Container(
-                    height: 50,
-                    width: 200,
-                    color: Color.fromARGB(255, 248, 1, 1),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    playSound(7);
-                  },
-                  child: Container(
-                    height: 50,
-                    width: 200,
-                    color: Color.fromARGB(255, 1, 113, 16),
-                  ),
-                ),
+                buildKey(color: Colors.red, soundNumber: 1),
+                buildKey(color: Colors.yellow, soundNumber: 2),
+                buildKey(color: Colors.green, soundNumber: 3),
+                buildKey(color: Colors.blue, soundNumber: 4),
+                buildKey(color: Colors.white, soundNumber: 5),
+                buildKey(color: Colors.orange, soundNumber: 6),
+                buildKey(color: Colors.purple, soundNumber: 7),
               ],
             ),
           ),
